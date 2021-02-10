9542af346432ee67c6da64360366aef1014f85dc
Asel <asel.akmatova@it-academy.kg>
2021-02-10 19:25:12 +0600

    add feature js and css create task

--- index.js
+++ index.js
@@ -23,3 +23,34 @@ btnFormOpen.addEventListener('click', () => {
     overlay.classList.remove('overlay_opened');
   });
 });
+
+taskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  taskForm.classList.remove('task-form_opened');
+  overlay.classList.remove('overlay_opened');
+
+  const taskTitle = document.getElementById('task-title').value;
+  const taskText = document.getElementById('task-text').value;
+
+  const tasksBox = document.querySelector('.box');
+  const task = document.createElement('div');
+  task.classList.add('box__task');
+
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.classList.add('check-box');
+
+  const trushIcon = document.createElement('a');
+  trushIcon.classList.add('delete-icon');
+  trushIcon.innerHTML = `<img  src="./images/delete.svg" alt="delete" />`;
+
+  task.innerHTML = `
+  <h3 class = "title-task">${taskTitle}</h3>
+  <p class = "text-task">${taskText}</p>`;
+
+  task.append(trushIcon);
+  task.append(checkBox);
+
+  tasksBox.prepend(task);
+});
